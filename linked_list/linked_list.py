15365ce8ca556e23ac27775f2797f21562a6fb07
Gediminas Z <gediminas.zubovicius@gmail.com>
2025-02-04 16:21:02 +0000

    Adding linked list pop method

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -12,6 +12,9 @@ class LinkedList:
             self.tail = None
             self.length = 0
 
+    def __len__(self):
+        return self.length
+
     def push(self, value):
         new_node = Node(value)
         if self.head is None:
@@ -30,3 +33,22 @@ class LinkedList:
             current_node = current_node.next
 
         return ' -> '.join(nodes)
+    
+    def pop(self):
+        if self.head is None:
+            return None
+        elif self.head == self.tail:
+            value = self.head.value
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return value
+        else:
+            current_node = self.head
+            while current_node.next != self.tail:
+                current_node = current_node.next
+            value = self.tail.value
+            self.tail = current_node
+            self.tail.next = None
+            self.length -= 1
+            return value
